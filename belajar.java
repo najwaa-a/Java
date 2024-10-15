a44bf3822b71d8004eae645cb38259817bad88da
najwaa-a <najwaareefa@gmail.com>
2024-10-15 16:45:45 +0700

    Update Switch

---
+++ belajar.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class belajar {
+
+    public static void main(String[] args) {
+        Scanner nilai = new Scanner(System.in);
+
+        System.out.println("Masukkan nilai: ");
+        int x = nilai.nextInt();
+
+        switch (x) {
+            case 1:
+                System.out.println("SATU");
+                break;
+            case 2:
+                System.out.println("DUA");
+                break;
+            case 3:
+                System.out.println("TIGA");
+                break;
+
+            default:
+                System.out.println("HAHAHAHA");
+                break;
+        }
+
+    }
+
+}
